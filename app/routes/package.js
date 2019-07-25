508d4b631d14eba470641f08af6dc951d0e54815
Bryce Mecum <petridish@gmail.com>
2019-07-24 22:06:48 -0800

    Use . syntax instead of [] to bemore consistent

--- app/routes/package.js
+++ app/routes/package.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.get('store').findRecord('package', params['id']);
+    return this.get('store').findRecord('package', params.id);
   }
 });
